cf026dfb7d57b1d228dd92fae3c476c7b520dc3b
pinmode-by <54795176+pinmode-by@users.noreply.github.com>
2020-05-11 12:43:32 +0300

    Add files via upload

---
+++ LFR/LFR_pr8.ino
@@ -0,0 +1,89 @@
+// Line Folower Robot (STEMBRIDGE)
+// controller : Arduino nano
+// driver     : TB6612
+//
+
+// motor's pins
+const byte AIN1 = 7;    // direction of left motor
+const byte AIN2 = 4;    // direction of left motor
+const byte BIN1 = 8;    // direction of right motor
+const byte BIN2 = 9;    // direction of right motor
+const byte PWMA = 5;    // left motor PWM
+const byte PWMB = 6;    // right motor PWM
+
+const byte BUTTON = 10;
+const byte NUM_SENS = 8;
+
+// pins PCB for Analog line
+byte pins[] = {A7, A6, A5, A4, A3, A2, A1, A0};
+int sensors[NUM_SENS];
+
+// functions declaration
+void drive(int left, int right);
+void pinsSetup();
+void readSensors();
+
+void setup() {
+  Serial.begin(9600);
+  pinsSetup();
+  while (digitalRead(BUTTON));
+  delay(500);
+}
+
+// 11.5м за 7.5с
+const float Kp = 37;
+const int SPEED = 180;
+int   Ws[] = { -7, -5, -3, -1, 1, 3, 5, 7};
+
+void loop() {
+  readSensors();
+  int sum = 0;
+  int sW = 0;
+  for (int i = 0; i < 8; ++i) {
+    sum += sensors[i];
+    sW += sensors[i] * Ws[i];
+  }
+  int delta = ((sW ) / (float) sum) * Kp;
+  drive(SPEED + delta, SPEED - delta);
+}
+
+
+void drive(int left, int right) {
+  left = constrain(left, -255, 255);
+  right = constrain(right, -255, 255);
+
+  if (left >= 0) {
+    digitalWrite(AIN1, LOW);
+    digitalWrite(AIN2, HIGH);
+  }
+  else {
+    digitalWrite(AIN1, HIGH);
+    digitalWrite(AIN2, LOW);
+  }
+
+  if (right >= 0) {
+    digitalWrite(BIN1, LOW);
+    digitalWrite(BIN2, HIGH);
+  }
+  else {
+    digitalWrite(BIN1, HIGH);
+    digitalWrite(BIN2, LOW);
+  }
+  analogWrite(PWMA, abs(left));
+  analogWrite(PWMB, abs(right));
+} // drive
+
+
+void pinsSetup() {
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  pinMode(BUTTON, INPUT_PULLUP);
+}
+
+void readSensors() {
+  for (int i = 0; i < NUM_SENS; ++i) {
+    sensors[i] = analogRead(pins[i]);
+  }
+}
